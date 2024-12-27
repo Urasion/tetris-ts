a43f1bf20240f901f2e05fae4b78a50e476ae876
Jeong Ji Won <wldnjs9705@penta.co.kr>
2024-12-27 09:54:29 +0900

    fix:minor

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -1,17 +1,11 @@
 import MainLayout from './MainLayout';
 import StartPage from '../../pages/StartPage';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
 
 export default function RootLayout() {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <MainLayout />,
-    },
-    {
-      path: '/start',
-      element: <StartPage />,
-    },
-  ]);
-  return <RouterProvider router={router}></RouterProvider>;
+  return (
+    <div className="w-full h-full">
+      <Outlet />
+    </div>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import RootLayout from './components/layout/RootLayout';
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import MainLayout from './components/layout/MainLayout';
+import StartPage from './pages/StartPage';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        path: 'tetris',
+        element: <MainLayout />,
+        children: [
+          {
+            path: '/start',
+            element: <StartPage />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <RootLayout />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
